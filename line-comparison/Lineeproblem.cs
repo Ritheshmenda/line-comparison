297a9884131aee315b325638d9e27442b7119b83
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-21 19:37:01 +0530

    [Rithesh]added:introduction

---
+++ line-comparison/Lineeproblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lineproblem
+{
+    class Lineeproblem
+    {
+        public void Introduction()
+        {
+            Console.WriteLine("welcome to the employee wage problem");
+        }
+    }
+}
---
+++ line-comparison/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lineproblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Lineeproblem Line = new Lineeproblem();
+            Line.Introduction();
+        }
+    }
+}
