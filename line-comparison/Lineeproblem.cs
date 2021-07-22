811ea100e6105e3a91664954be3b0febf21cf98a
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-22 15:27:44 +0530

    [rithesh]added:lineproblem

--- line-comparison/Lineeproblem.cs
+++ line-comparison/Lineeproblem.cs
@@ -6,7 +6,7 @@ namespace lineproblem
 {
     class lineproblem
     {
-        int x1, x2, x3, x4, y1, y2, y3, y4;
+        int x1, x2, x3, x4;
         public lineproblem(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
