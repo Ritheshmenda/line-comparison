68dd71371bb17d81ef7fe5c638f992c3f86b65db
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-23 07:20:53 +0530

    [rithesh]added:twolines

--- line-comparison/Lineeproblem.cs
+++ line-comparison/Lineeproblem.cs
@@ -6,25 +6,19 @@ namespace lineproblem
 {
     class lineproblem
     {
-        int x1, x2, x3, x4, y1, y2, y3, y4;
-        public lineproblem(int x1, int x2, int y1, int y2,int x3, int x4, int y3, int y4)
-        {
-            this.x1 = x1;
-            this.x2 = x2;
-            this.y1 = y1;
-            this.y2 = y2;
-            double lengthofline = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("length of line is:"+lengthofline);
-            this.x3 = x3;
-            this.x4 = x4;
-            this.y3 = y3;
-            this.y4 = y4;
-            double lengthofline_a = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
-            Console.WriteLine("length of line a is :" + lengthofline_a);
-            if (lengthofline == lengthofline_a)
-                Console.WriteLine("lines are equal");
-            else
-                Console.WriteLine("lines are not equal");
-        }
+       
+            int x1, x2, y1, y2;
+            public lineproblem(int x1, int x2, int y1, int y2)
+            {
+                this.x1 = x1;
+                this.x2 = x2;
+                this.y1 = y1;
+                this.y2 = y2;
+            }
+            public double Linecomparison()
+            {
+                double lengthofline = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                return lengthofline;
+            }
     }
 }
--- line-comparison/Program.cs
+++ line-comparison/Program.cs
@@ -6,8 +6,17 @@ namespace lineproblem
     {
         static void Main(string[] args)
         {
-            lineproblem Line = new lineproblem(1,7,4,8,5,1,9,7);
-           
+            lineproblem Line1 = new lineproblem(1, 7, 4, 8);
+            double Length1 = Line1.Linecomparison();
+            lineproblem Line2 = new lineproblem(8, 4, 7, 1);
+            double Length2 = Line2.Linecomparison();
+
+            Console.WriteLine("length of line 2 =" + Length2);
+            if (Length1 == Length2)
+                Console.WriteLine("Result:Both lines are equal");
+            else
+                Console.WriteLine("Result:both lines are not equal");
+
         }
     }
 }
