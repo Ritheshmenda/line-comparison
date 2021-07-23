54b21eca9855309e53f0d93bd53e3f648872e138
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-23 07:36:01 +0530

    [rithesh]added:main

--- line-comparison/Program.cs
+++ line-comparison/Program.cs
@@ -6,15 +6,7 @@ namespace lineproblem
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            lineproblem Line = new lineproblem(1,7,4,8,5,1,9,4);
-           
-=======
-            lineproblem Line1 = new lineproblem(1,7,4,8);
-            double Length1 = Line1.Linecomparison();
-            Console.WriteLine("length of line 1 =" + Length1);
-
->>>>>>> uc1-lineproblem
+           lineproblem Line = new lineproblem();                               
         }
     }
 }
