158c605de2c07751e06d91516d65275bf8cf99df
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-23 07:07:51 +0530

    [rithesh]added:lineproblem

--- line-comparison/Lineeproblem.cs
+++ line-comparison/Lineeproblem.cs
@@ -6,15 +6,18 @@ namespace lineproblem
 {
     class lineproblem
     {
-        int x1, x2, x3, x4;
+        int x1, x2, y1, y2;
         public lineproblem(int x1, int x2, int y1, int y2)
         {
             this.x1 = x1;
             this.x2 = x2;
             this.y1 = y1;
             this.y2 = y2;
-            double lengthofline = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("length of line is:"+lengthofline);
+        }
+        public double Linecomparison()
+        { 
+            double lengthofline = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));         
+            return lengthofline;
         }
     }
 }
--- line-comparison/Program.cs
+++ line-comparison/Program.cs
@@ -6,8 +6,10 @@ namespace lineproblem
     {
         static void Main(string[] args)
         {
-            lineproblem Line = new lineproblem(1,7,4,8);
-           
+            lineproblem Line1 = new lineproblem(1,7,4,8);
+            double Length1 = Line1.Linecomparison();
+            Console.WriteLine("length of line 1 =" + Length1);
+
         }
     }
 }
